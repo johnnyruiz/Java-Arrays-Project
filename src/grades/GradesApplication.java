ba80ec11e59d73a280547c10fe6617a91d6ee424
arleneamericamartinez <hubacek1221>
2019-05-23 14:05:02 -0500

    idk

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,32 +1,18 @@
 package grades;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class GradesApplication {
 
-public static String listusernames(HashMap Hm){
 
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Which student's Grades would you like to view?");
-    Hm.forEach((key,value) -> System.out.print(" ["+key+"] "));
-    System.out.print("\n");
-
-    String userResponse = scan.nextLine();
-
-    Hm.forEach((key,value) -> {
-        if (userResponse.equals(key)){
-            System.out.println(value.);
-            return userResponse;
-        }else{
-
-            listusernames(Hm);
-            return userResponse;
+    public static String has(ArrayList array){
+        for (int i = 0; i < array.size();i++) {
+            return array.get(i).toString();
         }
-
-    });
-}
-
+            return "error";
+    }
 
     public static void main(String[] args) {
 
@@ -45,19 +31,27 @@ public static String listusernames(HashMap Hm){
         synthia.addGrade(63,23,98,87,56);
         cloey.addGrade(85,47,100,74,94);
         ted.addGrade(85,84,96,63,100);
-//        students.forEach((key,value) -> System.out.println("Username :"+key));
-
-
+        ArrayList<String> users = new ArrayList<>();
         System.out.println("Hello User");
-        listusernames(students);
-        students.forEach((key,value) -> {
-            if (userResponse.equals(key)){
-                System.out.println(value.);
-            }else{
-                listusernames(students);
-            }
+        System.out.println("Which student's Grades would you like to view?");
+        String userResponse;
+        students.forEach((key,value) -> users.add(key));
 
-        });
+        for (int i = 0; i < users.size();i++) {
+            System.out.println(users.get(i));
+        }
+
+        do{
+            students.forEach((key, value) -> System.out.print(" [" + key + "] "));
+            userResponse = scan.nextLine();
+            System.out.println(students.get(userResponse).getGradeAverage());
+
+        }while (!userResponse.equals(has(users)));
+
+        students.get(userResponse).getGradeAverage();
+        System.out.println(students.get(userResponse).getGradeAverage());
+
+
 
 
 
